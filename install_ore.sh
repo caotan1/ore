f26f4b09c41988d8f43655134b6e5377866ccbed
caotan1 <ct_aoligei@outlook.com>
2024-10-14 19:26:30 +0800

    Update install_ore.sh

--- install_ore.sh
+++ install_ore.sh
@@ -154,7 +154,6 @@ sudo pkill -9 ore-miner
 sudo systemctl disable ore.service
 sudo rm -r /etc/systemd/system/ore.service
 sudo systemctl daemon-reload
-cd \
 sudo rm -r /home/ubuntu/ore
 ' > $TARGET_DIR/uninstall_ore.sh
 chmod +x $TARGET_DIR/uninstall_ore.sh
